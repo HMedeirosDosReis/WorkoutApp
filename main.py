fa608226d7de3ad92af146a4f4408fb52d2955ed
gabiMiranda <gabi.miranda15@hotmail.com>
2022-06-22 15:19:48 -0300

    started menu

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 #! /usr/bin/env python3
 
 from user import * 
+def choices():
+    print("Welcome to Workout App!")
+
+    typeOfUser = input("Are you an instructor or a member? "+"\n")
+
+    #memberList = ["Workout A","Workout B", "", ]
+    if typeOfUser=="member":
+        accessWorkouts = input("Would you like to access your workouts? (yes/no)"+"\n")
+    
+        if accessWorkouts == "yes":
+            print("Done!!")
+            ## workout class
+    
+        else:
+            print("...")
+
+
 
 def main():
     testUser = user("Prof1", "HM", "1011","henr", "med", "workA", "workB", "gain")
-    testUser.sp_print()
+#    testUser.sp_print()
 
 
 if __name__ == "__main__":
     main()
-    #gabiGata
+    choices()

eb7c7c93345bdcbee61bacf7f940397481a9d0a6
gabiMiranda <gabi.miranda15@hotmail.com>
2022-06-22 16:07:55 -0300

    added menu for instructor

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def choices():
     print("Welcome to Workout App!")
 
     typeOfUser = input("Are you an instructor or a member? "+"\n")
-
-    #memberList = ["Workout A","Workout B", "", ]
+    instructorList = ["add workout","remove workout","check progress","alter workout","add member","assign workout"]
+    
+   #memberList = ["Workout A","Workout B", "", ]
     if typeOfUser=="member":
         accessWorkouts = input("Would you like to access your workouts? (yes/no)"+"\n")
     
@@ -16,8 +17,24 @@ def choices():
     
         else:
             print("...")
-
-
+    
+    elif typeOfUser == "instructor": 
+        print("Here is a list of available options to be accessed: "+", ".join(instructorList)+
+                ".")
+        option =input( "Which option would you like to acces?"+"\n")
+        
+        if option==instructorList[0]:
+           print("...")
+        elif option==instructorList[1]:
+           print("...")
+        elif option==instructorList[2]:
+           print("...")
+        elif option==instructorList[3]:
+           print("...")
+        elif option==instructorList[4]:
+           print("...")
+        elif option==instructorList[5]:
+           print("...")
 
 def main():
     testUser = user("Prof1", "HM", "1011","henr", "med", "workA", "workB", "gain")
